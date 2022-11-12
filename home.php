3bac769ed4cae392b0e416655d2bbc083040b301
AnastasijaCv99 <acvetkovic99.ac@gmail.com>
2022-11-13 00:18:20 +0100

    dodat logout

--- home.php
+++ home.php
@@ -1,5 +1,27 @@
 <?php
 
-echo "prboa";
+//require("dbBroker.php");
+//require("model/user.php");
+//require("register.php");
+
+echo "bravo hehe";
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+    <title>Make your TO-DO list</title>
+</head>
+
+
+<body> 
+    <h1>Napravi svoju to-do listu </h1>
+    <h3><a href="logout.php">odjavi se</a></h3>
+</body>
+
+</html>
