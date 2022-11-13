f47c10441dcc3fb93896e1c0f285fb8ef65c7877
AnastasijaCv99 <acvetkovic99.ac@gmail.com>
2022-11-13 03:53:43 +0100

    css for index page

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
-<?php 
-
-echo "pocetna";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
-    <title>Make your TO-DO list</title>
+    <link rel="stylesheet" href="css/index.css">
+    <title>TO-DO list</title>
 
 </head>
 
 <body>
-        <h3><a href="login.php">PRIJAVI SE</a></h3>
+    
+
+    <div class="split left"> 
+    <h2> Napravi svoju To-Do listu! </h2>
+        <label for="prijava" id="prijava"><p> Imaš nalog? Prijavi se: </p></label><br>
+        <a href="login.php"> <button type="submit" class="btn" name="submit">PRIJAVI SE</button> </a>
         <br>
-        <h3><a href="register.php">REGISTRUJ SE</a></h3>
+        <br>
+        <label for="registracija" id="registracija"> <p> Nemaš nalog? Registruj se: </p> </label><br>
+        <a href="register.php"> <button type="submit" class="btn" name="submit"> REGISTRUJ SE</button> </a>
+    </div>
+     
+    <div class="split right"> 
+        <div class="centered">
+            <img src="images/logo.png" alt="logo" style="width:300px;height:300px;">
+            <p id="opis"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum optio consequatur perferendis, maxime soluta deleniti repellendus ex nam aspernatur! Fugit reprehenderit repellat molestias repudiandae iusto magni aperiam earum ullam labore? </p>
+    
+        </div>
+    </div>
+        
 
 </body>
 
